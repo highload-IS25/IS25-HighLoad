90e57a29ea398935fdf34803ae06bcd6bcc56c5b
vvvkks <kurilova2003@gmail.com>
2023-10-20 14:34:49 +0300

    add: KeyData class with id, key, and value fields for database operations

--- storage/src/main/java/com/example/storage/Entities/KeyData.java
+++ storage/src/main/java/com/example/storage/Entities/KeyData.java
@@ -1,17 +1,23 @@
 package com.example.storage.Entities;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 
 @Entity
 @AllArgsConstructor
+@Table(name = "INFO")
+@Data
 public class KeyData {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String key;
     private String value;
 
-    public KeyData(){}
+    public KeyData() {
+
+    }
 }
